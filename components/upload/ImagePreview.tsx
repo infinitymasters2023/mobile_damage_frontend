38bdff0e2b2b2721ad22fd572338c9c8db45a02a
infinitymasters2023 <144238563+infinitymasters2023@users.noreply.github.com>
2026-03-02 16:54:14 +0530

    fix: update ImagePreview component

--- components/upload/ImagePreview.tsx
+++ components/upload/ImagePreview.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import Image from "next/image";
-import { UploadedImage } from "@/app/page";
+import { UploadedImage } from "./upload";
+
 
 interface ImagePreviewProps {
   files: UploadedImage[];
